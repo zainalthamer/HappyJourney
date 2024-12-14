0fe9e4785cdea5d27ddf7066c000cebf8638753a
Zainab <202201685@student.polytechnic.bh>
2024-12-14 15:47:43 +0300

    Commit

--- HappyJourney/HappyJourney/Login.Designer.cs
+++ HappyJourney/HappyJourney/Login.Designer.cs
@@ -31,18 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblLogin = new System.Windows.Forms.Label();
-            this.txtBoxEmail = new System.Windows.Forms.TextBox();
-            this.txtBoxPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lblSignupLink = new System.Windows.Forms.LinkLabel();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(588, 89);
+            this.pictureBox1.Location = new System.Drawing.Point(588, 96);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(308, 147);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -53,36 +53,18 @@
             // 
             this.lblLogin.AutoSize = true;
             this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.Location = new System.Drawing.Point(693, 253);
+            this.lblLogin.Location = new System.Drawing.Point(693, 260);
             this.lblLogin.Name = "lblLogin";
             this.lblLogin.Size = new System.Drawing.Size(101, 37);
             this.lblLogin.TabIndex = 3;
             this.lblLogin.Text = "Login";
-            // 
-            // txtBoxEmail
-            // 
-            this.txtBoxEmail.Location = new System.Drawing.Point(570, 336);
-            this.txtBoxEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtBoxEmail.MinimumSize = new System.Drawing.Size(4, 26);
-            this.txtBoxEmail.Name = "txtBoxEmail";
-            this.txtBoxEmail.Size = new System.Drawing.Size(349, 31);
-            this.txtBoxEmail.TabIndex = 4;
-            // 
-            // txtBoxPassword
-            // 
-            this.txtBoxPassword.Location = new System.Drawing.Point(570, 408);
-            this.txtBoxPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtBoxPassword.MinimumSize = new System.Drawing.Size(4, 26);
-            this.txtBoxPassword.Name = "txtBoxPassword";
-            this.txtBoxPassword.Size = new System.Drawing.Size(349, 31);
-            this.txtBoxPassword.TabIndex = 5;
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.Black;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(657, 478);
+            this.btnLogin.Location = new System.Drawing.Point(657, 485);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(158, 56);
             this.btnLogin.TabIndex = 11;
@@ -93,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(579, 562);
+            this.label1.Location = new System.Drawing.Point(579, 569);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(238, 25);
             this.label1.TabIndex = 12;
@@ -103,7 +85,7 @@
             // 
             this.lblSignupLink.AutoSize = true;
             this.lblSignupLink.LinkColor = System.Drawing.Color.RoyalBlue;
-            this.lblSignupLink.Location = new System.Drawing.Point(804, 562);
+            this.lblSignupLink.Location = new System.Drawing.Point(804, 569);
             this.lblSignupLink.Name = "lblSignupLink";
             this.lblSignupLink.Size = new System.Drawing.Size(79, 25);
             this.lblSignupLink.TabIndex = 13;
@@ -111,17 +93,31 @@
             this.lblSignupLink.Text = "Signup";
             this.lblSignupLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblSignupLink_LinkClicked);
             // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(541, 406);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(418, 31);
+            this.txtBoxPassword.TabIndex = 15;
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(541, 350);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(418, 31);
+            this.txtBoxEmail.TabIndex = 14;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1506, 703);
+            this.ClientSize = new System.Drawing.Size(1472, 719);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxEmail);
             this.Controls.Add(this.lblSignupLink);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtBoxPassword);
-            this.Controls.Add(this.txtBoxEmail);
             this.Controls.Add(this.lblLogin);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -138,10 +134,10 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblLogin;
-        private System.Windows.Forms.TextBox txtBoxEmail;
-        private System.Windows.Forms.TextBox txtBoxPassword;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel lblSignupLink;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.TextBox txtBoxEmail;
     }
 }
