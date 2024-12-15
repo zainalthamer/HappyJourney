a639c6e422b00bcab8742ecb142f24b3e9ae017a
Zainab <202201685@student.polytechnic.bh>
2024-12-15 12:36:32 +0300

    Added compose screen logic

--- HappyJourney/HappyJourney/Compose.Designer.cs
+++ HappyJourney/HappyJourney/Compose.Designer.cs
@@ -28,28 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuCompose = new System.Windows.Forms.MenuStrip();
             this.lblNewMessage = new System.Windows.Forms.Label();
             this.txtRecepient = new System.Windows.Forms.TextBox();
             this.txtMessageContent = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
+            this.chkBroadcast = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // mnuCompose
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1472, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.mnuCompose.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.mnuCompose.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.mnuCompose.Location = new System.Drawing.Point(0, 0);
+            this.mnuCompose.Name = "mnuCompose";
+            this.mnuCompose.Size = new System.Drawing.Size(1472, 24);
+            this.mnuCompose.TabIndex = 0;
+            this.mnuCompose.Text = "menuStrip1";
             // 
             // lblNewMessage
             // 
             this.lblNewMessage.AutoSize = true;
             this.lblNewMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNewMessage.Location = new System.Drawing.Point(647, 142);
+            this.lblNewMessage.Location = new System.Drawing.Point(647, 127);
             this.lblNewMessage.Name = "lblNewMessage";
             this.lblNewMessage.Size = new System.Drawing.Size(229, 37);
             this.lblNewMessage.TabIndex = 2;
@@ -57,14 +58,14 @@
             // 
             // txtRecepient
             // 
-            this.txtRecepient.Location = new System.Drawing.Point(558, 218);
+            this.txtRecepient.Location = new System.Drawing.Point(558, 203);
             this.txtRecepient.Name = "txtRecepient";
             this.txtRecepient.Size = new System.Drawing.Size(397, 31);
             this.txtRecepient.TabIndex = 3;
             // 
             // txtMessageContent
             // 
-            this.txtMessageContent.Location = new System.Drawing.Point(558, 275);
+            this.txtMessageContent.Location = new System.Drawing.Point(558, 260);
             this.txtMessageContent.Multiline = true;
             this.txtMessageContent.Name = "txtMessageContent";
             this.txtMessageContent.Size = new System.Drawing.Size(397, 263);
@@ -76,12 +77,23 @@
             this.btnSend.BackColor = System.Drawing.Color.Black;
             this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSend.ForeColor = System.Drawing.Color.White;
-            this.btnSend.Location = new System.Drawing.Point(670, 565);
+            this.btnSend.Location = new System.Drawing.Point(670, 607);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(181, 43);
             this.btnSend.TabIndex = 5;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // chkBroadcast
+            // 
+            this.chkBroadcast.AutoSize = true;
+            this.chkBroadcast.Location = new System.Drawing.Point(558, 551);
+            this.chkBroadcast.Name = "chkBroadcast";
+            this.chkBroadcast.Size = new System.Drawing.Size(141, 29);
+            this.chkBroadcast.TabIndex = 6;
+            this.chkBroadcast.Text = "Broadcast";
+            this.chkBroadcast.UseVisualStyleBackColor = true;
             // 
             // Compose
             // 
@@ -89,12 +101,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1472, 719);
+            this.Controls.Add(this.chkBroadcast);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtMessageContent);
             this.Controls.Add(this.txtRecepient);
             this.Controls.Add(this.lblNewMessage);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.mnuCompose);
+            this.MainMenuStrip = this.mnuCompose;
             this.Name = "Compose";
             this.Text = "Compose";
             this.Load += new System.EventHandler(this.Compose_Load);
@@ -105,10 +118,11 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip mnuCompose;
         private System.Windows.Forms.Label lblNewMessage;
         private System.Windows.Forms.TextBox txtRecepient;
         private System.Windows.Forms.TextBox txtMessageContent;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.CheckBox chkBroadcast;
     }
 }
--- HappyJourney/HappyJourney/Compose.cs
+++ HappyJourney/HappyJourney/Compose.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace HappyJourney
         private int loggedInUserId;
         private int loggedInUserRoleId;
 
-        public Compose(int userId,int roleId)
+        public Compose(int userId, int roleId)
         {
             InitializeComponent();
             loggedInUserId = userId;
@@ -28,7 +29,10 @@ namespace HappyJourney
 
         private void Compose_Load(object sender, EventArgs e)
         {
-
+            if (loggedInUserRoleId != 1)
+            {
+                chkBroadcast.Visible = false;
+            }
         }
 
         private void SetupPlaceholder(TextBox textBox, string placeholderText)
@@ -56,6 +60,106 @@ namespace HappyJourney
                     textBox.ForeColor = Color.LightGray; // Set placeholder color
                 }
             };
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRecepient.Text) && !chkBroadcast.Checked)
+            {
+                MessageBox.Show("Please specify a recipient.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtMessageContent.Text))
+            {
+                MessageBox.Show("Message content cannot be empty.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string messageContent = txtMessageContent.Text;
+            DateTime currentDate = DateTime.Now;
+
+            try
+            {
+                if (chkBroadcast.Checked && loggedInUserRoleId == 1)
+                {
+                    BroadcastMessage(messageContent, currentDate);
+                }
+                else
+                {
+                    SendMessage(messageContent, currentDate, txtRecepient.Text);
+                }
+
+                MessageBox.Show("Message sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BroadcastMessage(string messageContent, DateTime messageDate)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\zainn\\OneDrive\\Desktop\\HappyJourney\\HappyJourney\\HappyJourney\\happy_journey.mdf;Integrated Security=True;Connect Timeout=30";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string fetchUsersQuery = "SELECT user_id FROM [User] WHERE user_id != @SenderId";
+                SqlCommand fetchCommand = new SqlCommand(fetchUsersQuery, connection);
+                fetchCommand.Parameters.AddWithValue("@SenderId", loggedInUserId);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(fetchCommand);
+                DataTable userTable = new DataTable();
+                adapter.Fill(userTable);
+
+                foreach (DataRow row in userTable.Rows)
+                {
+                    int recipientId = Convert.ToInt32(row["user_id"]);
+                    InsertMessage(connection, messageContent, messageDate, recipientId);
+                }
+            }
+        }
+
+        private void SendMessage(string messageContent, DateTime messageDate, string recipientEmail)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\zainn\\OneDrive\\Desktop\\HappyJourney\\HappyJourney\\HappyJourney\\happy_journey.mdf;Integrated Security=True;Connect Timeout=30";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string fetchRecepientQuery = "SELECT user_id FROM [User] WHERE email = @Email";
+                SqlCommand fetchCommand = new SqlCommand(fetchRecepientQuery, connection);
+                fetchCommand.Parameters.AddWithValue("@Email", recipientEmail);
+
+                object recipientIdObj = fetchCommand.ExecuteScalar();
+
+                if (recipientIdObj != null)
+                {
+                    int recipientId = Convert.ToInt32(recipientIdObj);
+                    InsertMessage(connection, messageContent, messageDate, recipientId);
+                }
+                else
+                {
+                    throw new Exception("Recipient email not found.");
+                }
+            }
+        }
+
+        private void InsertMessage(SqlConnection connection, string content, DateTime date, int receiverId)
+        {
+            string insertMessageQuery = "INSERT INTO Message (content, sender_id, receiver_id, date) " +
+                                        "VALUES (@Content, @SenderId, @ReceiverId, @Date)";
+
+            SqlCommand insertCommand = new SqlCommand(insertMessageQuery, connection);
+            insertCommand.Parameters.AddWithValue("@Content", content);
+            insertCommand.Parameters.AddWithValue("@SenderId", loggedInUserId);
+            insertCommand.Parameters.AddWithValue("@ReceiverId", receiverId);
+            insertCommand.Parameters.AddWithValue("@Date", date);
+
+            insertCommand.ExecuteNonQuery();
         }
     }
 }
